34ef8fd60d4177b81b3eb46cc30689ccdaea59d4
ankur-cr-esper <ankur-cr@esper.io>
2023-10-03 17:41:06 +0530

    updated python code

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ s3 = boto3.resource('s3')
                 #  aws_access_key_id='MY_AWS_KEY_ID',
                 #  aws_secret_access_key='MY_AWS_SECRET_ACCESS_KEY'
                 
-json_object =  s3.Object('demo-bucket-acs', 'version.json')
-json_object["image_version"] = "2.2.0"
+bucket_name = 'demo-bucket-acs'
+object_key = 'version.json'
+json_object =  s3.Object(bucket_name,object_key)
+metadata = {'image_version': '2.2.0'}
+json_object.copy_from(CopySource={'Bucket': bucket_name, 'Key': object_key}, Metadata=metadata, MetadataDirective='REPLACE')
 print(json_object)

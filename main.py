07f94c06e50a58e40b76b3ac38db832bf70faf46
ankur-cr-esper <ankur-cr@esper.io>
2023-10-03 17:58:39 +0530

    updated python code

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ s3 = boto3.resource('s3')
 bucket_name = 'demo-bucket-acs'
 object_key = 'version.json'
 json_object =  s3.Object(bucket_name,object_key)
-metadata = {'image_version': '2.2.0'}
+metadata = {"image_version": "2.2.0"}
 json_object.copy_from(CopySource={'Bucket': bucket_name, 'Key': object_key}, Metadata=metadata, MetadataDirective='REPLACE')
-print(json_object)
+updated_metadata = json_object.metadata
+print(updated_metadata)

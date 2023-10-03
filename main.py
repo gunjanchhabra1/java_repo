8ce58cb3bc258dcf49d4c54ae288becaf6802df4
ankur-cr-esper <ankur-cr@esper.io>
2023-10-03 17:15:52 +0530

    updated python code

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import json
 import boto3
 
 s3 = boto3.resource('s3')
-json_client = boto3('s3',
+json_client = boto3('s3')
                 #  aws_access_key_id='MY_AWS_KEY_ID',
                 #  aws_secret_access_key='MY_AWS_SECRET_ACCESS_KEY'
-                )
+                
 json_object =  s3.Object('demo-bucket-acs', 'version.json')
 json_object["image_version"] = "2.2.0"
 print(json_object)
